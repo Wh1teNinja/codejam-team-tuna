c29f1d088d078223dc9fc11229cbc9afab0fab65
Andrei Fedchenko <afedchenko@guidewire.com>
2024-06-25 11:23:06 -0400

    added first pages

---
+++ frontend-app/src/pages/QuestionsPage/QuestionsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const QuestionsPage = (): React.JSX.Element => {
+    return <div>
+        <h1>Questions page!</h1>
+    </div>
+}
---
+++ frontend-app/src/pages/StartPage/StartPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const StartPage = (): React.JSX.Element => {
+    return <div>
+        <h1>Start page!</h1>
+    </div>
+}
